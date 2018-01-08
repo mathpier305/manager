bdf91411981e405dc79a9110a825b877faffc388
Pierre, Mathurin <mathurin.pierre@cengage.com>
2018-01-08 00:09:38 -0500

    set up environment for manager app

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import firebase from 'firebase';
+import reducers from './reducers';
+import LoginForm from './components/LoginForm';
+
+
+class App extends Component {
+    componentWillMount() {
+      const config = {
+        apiKey: 'AIzaSyDHP6PxL3qi656bakYyZ794cGVIy3JBrxI',
+        authDomain: 'manager-ad505.firebaseapp.com',
+        databaseURL: 'https://manager-ad505.firebaseio.com',
+        projectId: 'manager-ad505',
+        storageBucket: '',
+        messagingSenderId: '305684529093'
+      };
+      firebase.initializeApp(config);
+  }
+  render() {
+    return (
+      <Provider store={createStore(reducers)}>
+        <View>
+          <LoginForm />
+        </View>
+      </Provider>
+    );
+  }
+}
+
+export default App;
